2fa2c577c117219bb4fde7e8b6388d3d91b8f340
krystiano13 <crystianotv@gmail.com>
2024-01-02 20:18:22 +0100

    create Quizz model

---
+++ server/app/Models/Quizz.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quizz extends Model
+{
+    use HasFactory;
+
+    /**
+     * Table name
+     * @var string
+     */
+    protected $table = "quizzes";
+
+    /**
+     * Primary Key
+     * @var string
+     */
+    protected $primaryKey = "id";
+
+    /**
+     * Is incrementing
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * Fillable Elements
+     * @var string[]
+     */
+    protected $fillable = [
+        'author',
+        'title'
+    ];
+
+    /**
+     * Hidden Elements
+     * @var string[]
+     */
+    protected $hidden = [
+        'updated_at'
+    ];
+}
