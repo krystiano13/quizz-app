505ccef5f6d531f347b0739757273da57e0eebe3
krystiano13 <crystianotv@gmail.com>
2024-01-03 11:55:46 +0100

    create api function for searching quizzes

---
+++ server/app/Http/Controllers/QuizzController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Quizz;
+use function Laravel\Prompts\search;
+
+class QuizzController extends Controller
+{
+    public function search(Request $req) {
+        $validation = Validator::make($req -> all(),[
+            'search' => 'required'
+        ]);
+
+        if($validation -> fails()) {
+            return response([
+                'status' => false,
+                'errors' => $validation -> errors()
+            ], 403);
+        }
+
+        $text = $req -> get('search');
+        $result = Quizz::where('title','like','%'.$text.'%') -> get();
+
+        return response([
+            'status' => true,
+            'result' => $result
+        ], 200);
+    }
+}
