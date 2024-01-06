db6709c5f73cfbc049ab2ce85e4a0e2357d67762
krystiano13 <crystianotv@gmail.com>
2024-01-06 16:21:41 +0100

    implement getting data from search input

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from "react-router-dom";
 
 export function Home() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+        if(searchParams.get('search')) {
+            const data = new FormData();
+            data.append("search", searchParams.get('search') as string);
+            fetch('http://127.0.0.1:8000/api/quizz/search', {
+                method: "POST",
+                body: data
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+        }
+    }, []);
+
     return (
        <></>
     )
