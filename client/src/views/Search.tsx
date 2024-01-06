c23349341f2a63d4b1fd5450192412983a5f07aa
krystiano13 <crystianotv@gmail.com>
2024-01-06 16:15:30 +0100

    implement pushing result of search input into search params

--- client/src/views/Search.tsx
+++ client/src/views/Search.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+//imports
+import React, { useRef } from 'react';
+import {NavigateFunction, useNavigate} from "react-router";
+
+//components
 import { Input } from "../components/ui/input";
 import { Button } from '../components/ui/button';
 
 export default function Search() {
+    const navigate:NavigateFunction = useNavigate();
+    const inputRef = useRef<HTMLInputElement>(null);
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        const search:string = (inputRef.current as HTMLInputElement).value;
+        navigate(`/?search=${search}`);
+    }
+
     return (
         <main className="form-anim theme-rose w-[100vw] h-[100vh] flex justify-center items-center">
-            <form className="flex gap-1">
-                <Input className="text-lg p-6" placeholder="Search ..." />
+            <form onSubmit={handleSubmit} className="flex gap-1">
+                <Input
+                    ref={inputRef}
+                    name="search"
+                    required
+                    type="text"
+                    className="text-lg p-6"
+                    placeholder="Search ..."
+                />
                 <Button className="text-lg p-6" type="submit">Submit</Button>
             </form>
         </main>
