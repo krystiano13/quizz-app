d69d3c1cedc6440b1e594e64ccf6695856dc996e
krystiano13 <crystianotv@gmail.com>
2024-01-06 16:07:24 +0100

    create structure and styles for search view

--- client/src/views/Search.tsx
+++ client/src/views/Search.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Input } from "../components/ui/input";
+import { Button } from '../components/ui/button';
 
 export default function Search() {
     return (
-        <></>
+        <main className="form-anim theme-rose w-[100vw] h-[100vh] flex justify-center items-center">
+            <form className="flex gap-1">
+                <Input className="text-lg p-6" placeholder="Search ..." />
+                <Button className="text-lg p-6" type="submit">Submit</Button>
+            </form>
+        </main>
     )
 }
