166f35c00dbe94351cbac126c8b86e8a7005be13
krystiano13 <crystianotv@gmail.com>
2024-01-07 13:39:48 +0100

    add error handling for registration error

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { Root } from "./Root";
 import { ThemeProvider } from "./components/theme-provider"
+import { Toaster } from './components/ui/toaster';
 
 import './global.css';
 import './output.min.css';
@@ -13,6 +14,7 @@ App.render(
     <>
         <ThemeProvider>
             <Root />
+            <Toaster />
         </ThemeProvider>
     </>
 )
--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Card} from "../components/ui/card";
 import {Input} from "../components/ui/input";
 import {Button} from "../components/ui/button";
+import { useToast } from "../components/ui/use-toast";
 
 export default function Register() {
-
+    const { toast }  = useToast();
     async function handleSubmit(e:React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         const data = new FormData(e.target as HTMLFormElement);
@@ -13,16 +14,23 @@ export default function Register() {
             method: "POST",
             body: data
         })
-            .then(res => res.json())
+            .then(res => {
+                return res.json();
+            })
             .then(data => {
-
+               if(!data.status) {
+                   toast({
+                       title: "Couldn't create an account",
+                       description: "Validation error"
+                   });
+               }
             })
     }
 
     return (
         <main className="form-anim theme-rose w-[100vw] h-[100vh] flex justify-center items-center">
             <Card className="bg-card w-[70%] sm:w-[70%] md:w-[35%] lg:w-[28%] xl:w-[22%] 2xl:w-[20%]">
-                <form className="w-full pt-20 pb-20 pl-6 pr-6 flex flex-col items-center">
+                <form onSubmit={handleSubmit} className="w-full pt-20 pb-20 pl-6 pr-6 flex flex-col items-center">
                     <Input className="mt-4 mb-4 w-[80%]" type="text" placeholder="username" name="name" required/>
                     <Input className="mt-4 mb-4 w-[80%]" type="email" placeholder="email" name="email" required />
                     <Input className="mt-4 mb-4 w-[80%]" type="password" placeholder="password" name="password"
