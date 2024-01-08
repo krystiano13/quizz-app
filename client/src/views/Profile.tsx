ee502a26c61b548466730b8aa65951589ace4c2a
krystiano13 <crystianotv@gmail.com>
2024-01-08 11:04:03 +0100

    implement showing user profile in client

--- client/src/views/Profile.tsx
+++ client/src/views/Profile.tsx
@@ -18,8 +18,8 @@ interface UserInfo {
     created_at: string;
     updated_at: string;
     quizzes_made: number;
-    quizzes_solved: number;
     quizzes_rated: number;
+    quizzes_solved: number;
     average_rating: number;
 }
 
@@ -35,8 +35,22 @@ const Profile = () => {
 
         fetch(`http://127.0.0.1:8000/api/profile/${searchParams.get('username')}`)
             .then(res => res.json())
-            .then(data => {
-                console.log(data);
+            .then((data) => {
+                if(data.result.length < 1) {
+                    navigate('/');
+                    return;
+                }
+                setUserInfo([{
+                    id: data.result[0].id,
+                    name: data.result[0].name,
+                    about: data.result[0].about,
+                    created_at: data.result[0].created_at,
+                    updated_at: data.result[0].updated_at,
+                    quizzes_made: data.result[0].quizzes_made,
+                    quizzes_rated: data.result[0].quizzes_rated,
+                    quizzes_solved: data.result[0].quizzes_solved,
+                    average_rating: data.result[0].average_rating
+                }]);
             })
     }
 
@@ -48,20 +62,22 @@ const Profile = () => {
         <main className="form-anim theme-rose w-[100vw] h-[100vh] flex justify-center items-center">
             <Card className="p-6 flex flex-col w-[75%] md:w-[45%] lg:w-[35%] xl:w-[30%]">
                 <Avatar className="ml-3">
-                    <AvatarFallback>Ad</AvatarFallback>
+                    <AvatarFallback>
+                        { userInfo[0].name[0].toUpperCase() + userInfo[0].name[1] }
+                    </AvatarFallback>
                 </Avatar>
                 <CardHeader>
-                    <CardTitle>Admin</CardTitle>
+                    <CardTitle>{ userInfo[0].name }</CardTitle>
                     <CardDescription>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et felis lacus. Duis et mollis augue. Nam scelerisque ultrices ligula. Fusce congue condimentum nisl. Curabitur commodo eleifend risus sed vestibulum.
+                        { userInfo[0].about }
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
                     <ul>
-                        <li>Quizzes Made: 12</li>
-                        <li>Quizzes Solved: 40</li>
-                        <li>Quizzes Rated: 25</li>
-                        <li>Average Quizzes Ratings: 4.23</li>
+                        <li>Quizzes Made: { userInfo[0].quizzes_made }</li>
+                        <li>Quizzes Solved: { userInfo[0].quizzes_solved }</li>
+                        <li>Quizzes Rated: { userInfo[0].quizzes_rated }</li>
+                        <li>Average Quizzes Ratings: { userInfo[0].average_rating }</li>
                     </ul>
                 </CardContent>
             </Card>
