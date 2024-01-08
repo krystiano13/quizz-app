62c43b4beb2876c42947b3f37352ff8debaf2702
krystiano13 <crystianotv@gmail.com>
2024-01-08 12:31:55 +0100

    add textarea to about edit form

--- client/src/views/AboutEdit.tsx
+++ client/src/views/AboutEdit.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 // components
 import { Button } from "../components/ui/button";
 import { Card } from '../components/ui/card';
+import { Textarea } from '../components/ui/textarea';
 
 const AboutEdit = () => {
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -13,6 +14,7 @@ const AboutEdit = () => {
         <main className="form-anim theme-rose w-[100vw] h-[100vh] flex justify-center items-center">
             <Card className="bg-card w-[70%] md:w-[35%] lg:w-[28%] xl:w-[22%]">
                 <form onSubmit={handleSubmit} className="w-full pt-20 pb-20 pl-6 pr-6 flex flex-col items-center">
+                    <Textarea className="mt-4 mb-4 w-[80%]" placeholder="Your about section ..."></Textarea>
                     <Button className="mt-4 mb-4 w-[80%]" type="submit">Edit</Button>
                 </form>
             </Card>
