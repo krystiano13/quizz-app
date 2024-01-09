731daf7401579de01d0675b7407996389b497f3f
krystiano13 <crystianotv@gmail.com>
2024-01-09 14:00:22 +0100

    create structure for quizz preview view

--- client/src/views/QuizzPreview.tsx
+++ client/src/views/QuizzPreview.tsx
@@ -3,10 +3,41 @@ import React from 'react';
 // components
 import { Card, CardHeader, CardContent, CardTitle, CardDescription } from '../components/ui/card';
 import { Button } from '../components/ui/button';
-
+import { Separator } from '../components/ui/separator';
 
 export default function QuizzPreview() {
     return (
-        <></>
+        <main className="form-anim theme-rose w-[100vw] h-[100vh] flex justify-center items-center">
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        Test 1
+                    </CardTitle>
+                </CardHeader>
+                <Separator />
+                <CardContent>
+                    <CardDescription>
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit.
+                        Sed tempus purus at sem lobortis, et finibus eros aliquet.
+                        Nunc condimentum consectetur placerat.
+                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+                        Aenean sagittis, ex et egestas feugiat, augue ligula sagittis risus,
+                        a dignissim ante urna in est. Donec tincidunt dignissim lobortis.
+                    </CardDescription>
+                    <section id="rating">
+                        <h2>Rate Quizz:</h2>
+                        <div id="buttons">
+                            <Button className="rounded-full w-[2rem] h-[2rem] mr-2"></Button>
+                            <Button className="rounded-full w-[2rem] h-[2rem] mr-2"></Button>
+                            <Button className="rounded-full w-[2rem] h-[2rem] mr-2"></Button>
+                            <Button className="rounded-full w-[2rem] h-[2rem] mr-2"></Button>
+                            <Button className="rounded-full w-[2rem] h-[2rem] mr-2"></Button>
+                        </div>
+                    </section>
+                    <Button>Start Quizz</Button>
+                </CardContent>
+            </Card>
+        </main>
     )
 }
