cabdcd33f46a59bf663dd8ca7356f5848e2a0a82
krystiano13 <crystianotv@gmail.com>
2024-01-09 13:30:04 +0100

    create styles for mobile version of quizz view

--- client/src/views/Quizz.tsx
+++ client/src/views/Quizz.tsx
@@ -7,16 +7,16 @@ import { Button } from '../components/ui/button';
 export default function Quizz() {
     return (
         <main className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
-            <Card className="h-1/2 w-4/5 flex justify-center items-center">
-                <h1 className="text-xl font-bold max-w-[80%]">
+            <Card className="h-1/3 md:h-1/2 w-4/5 flex justify-center items-center">
+                <h1 className="text-lg md:text-xl text-center font-bold max-w-[80%]">
                     Test quizz question. Lorem ipsum dolor ...
                 </h1>
             </Card>
             <div id="answers" className="w-4/5 h-[30%] flex flex-wrap justify-between">
-                <Button className="w-1/2 h-1/2 text-lg" variant="outline">Answer A</Button>
-                <Button className="w-1/2 h-1/2 text-lg" variant="outline">Answer B</Button>
-                <Button className="w-1/2 h-1/2 text-lg" variant="outline">Answer C</Button>
-                <Button className="w-1/2 h-1/2 text-lg" variant="outline">Answer D</Button>
+                <Button className="w-[100%] md:w-1/2 h-1/4 md:h-1/2 text-base md:text-lg" variant="outline">Answer A</Button>
+                <Button className="w-[100%] md:w-1/2 h-1/4 md:h-1/2 text-base md:text-lg" variant="outline">Answer B</Button>
+                <Button className="w-[100%] md:w-1/2 h-1/4 md:h-1/2 text-base md:text-lg" variant="outline">Answer C</Button>
+                <Button className="w-[100%] md:w-1/2 h-1/4 md:h-1/2 text-base md:text-lg" variant="outline">Answer D</Button>
             </div>
         </main>
     )
