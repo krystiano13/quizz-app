89245c7aae370dacdf8b6e382f387425103e36ae
krystiano13 <crystianotv@gmail.com>
2024-01-09 13:19:02 +0100

    create basic structure for quizz view

--- client/src/views/Quizz.tsx
+++ client/src/views/Quizz.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+// components
+import { Card } from '../components/ui/card';
+
 export default function Quizz() {
     return (
-        <></>
+        <main className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
+            <Card className="h-1/2 w-4/5"></Card>
+            <div id="answers" className="w-4/5 h-[30%]"></div>
+        </main>
     )
 }
