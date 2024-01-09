fc3de383b73f588b9c24a03a1a17abe260dc09fd
krystiano13 <crystianotv@gmail.com>
2024-01-09 13:55:29 +0100

    add separator component

--- client/src/views/QuizzPreview.tsx
+++ client/src/views/QuizzPreview.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// components
+import { Card, CardHeader, CardContent, CardTitle, CardDescription } from '../components/ui/card';
+import { Button } from '../components/ui/button';
+
+
 export default function QuizzPreview() {
     return (
         <></>
