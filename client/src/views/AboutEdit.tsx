0eb6f0fef74e09aecdc57814e54bd180679d155b
krystiano13 <crystianotv@gmail.com>
2024-01-09 11:19:09 +0100

    implement getting actual version of about section from database to edit form

--- client/src/views/AboutEdit.tsx
+++ client/src/views/AboutEdit.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router";
 import Cookies from 'universal-cookie';
 
@@ -14,6 +14,8 @@ const AboutEdit = () => {
     const cookies = new Cookies();
     const navigate = useNavigate();
     const { toast } = useToast();
+
+    const [defaultValue, setDefaultValue] = useState<string>("");
 
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -38,17 +40,29 @@ const AboutEdit = () => {
             })
     }
 
+    function getDefaultValue() {
+        fetch(`http://127.0.0.1:8000/api/profile/about/${cookies.get('quizzapp_username')}`)
+            .then(res => res.json())
+            .then(data => {
+                if(data.status) {
+                    setDefaultValue(data.result[0].about);
+                }
+            })
+    }
+
     useEffect(() => {
         if(!cookies.get("quizzapp_username")) {
             navigate('/');
+            return;
         }
+        getDefaultValue();
     }, []);
 
     return (
         <main className="form-anim theme-rose w-[100vw] h-[100vh] flex justify-center items-center">
             <Card className="bg-card w-[70%] md:w-[35%] lg:w-[28%] xl:w-[22%]">
                 <form onSubmit={handleSubmit} className="w-full pt-20 pb-20 pl-6 pr-6 flex flex-col items-center">
-                    <Textarea name="about" className="mt-4 mb-4 w-[80%]" placeholder="Your about section ..."></Textarea>
+                    <Textarea defaultValue={defaultValue} name="about" className="mt-4 mb-4 w-[80%]" placeholder="Your about section ..."></Textarea>
                     <Button className="mt-4 mb-4 w-[80%]" type="submit">Edit</Button>
                 </form>
             </Card>
