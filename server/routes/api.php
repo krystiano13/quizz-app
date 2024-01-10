c227405255d85d9d8948df6a45b1f3b7a05a78c7
krystiano13 <crystianotv@gmail.com>
2024-01-10 20:52:48 +0100

    add route protection for post methods using laravel sanctum

--- server/routes/api.php
+++ server/routes/api.php
@@ -14,30 +14,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Ratings
 Route::get('/ratings/{username}/{id}', [RatingController::class, 'getRating']);
-Route::post('/ratings/edit/{id}', [RatingController::class, 'editRating']);
-Route::post('/ratings/add', [RatingController::class, 'addRating']);
+Route::middleware('auth:sanctum') -> post('/ratings/edit/{id}', [RatingController::class, 'editRating']);
+Route::middleware('auth:sanctum') -> post('/ratings/add', [RatingController::class, 'addRating']);
 
 //Profile
 Route::get('/profile/{username}', [ProfileController::class, 'get']);
 Route::get('/profile/about/{username}', [ProfileController::class, 'getAbout']);
-Route::post('/profile/edit', [ProfileController::class, 'editDescription']);
+Route::middleware('auth:sanctum') -> post('/profile/edit', [ProfileController::class, 'editDescription']);
 
 //Auth
-Route::post('/auth/register', [UserController::class, 'register']);
-Route::post('/auth/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum') -> post('/auth/register', [UserController::class, 'register']);
+Route::middleware('auth:sanctum') -> post('/auth/login', [UserController::class, 'login']);
 
 //Quizz API routes
 Route::get('/quizz/id/{id}', [QuizzController::class, 'getById']);
 Route::get('/quizz/latest', [QuizzController::class, 'getLatest']);
 Route::get('/quizz/highestRated', [QuizzController::class, 'getHighestRated']);
 Route::get('/quizz/user/{username}', [QuizzController::class, 'getUsersQuizzes']);
-Route::post('/quizz/search',[QuizzController::class, 'search']);
-Route::post('/quizz/delete/{id}', [QuizzController::class, 'deleteQuizz']);
-Route::post('/quizz/edit/{id}', [QuizzController::class, 'editQuizz']);
-Route::post('/quizz/add', [QuizzController::class, 'addQuizz']);
+Route::middleware('auth:sanctum') -> post('/quizz/search',[QuizzController::class, 'search']);
+Route::middleware('auth:sanctum') -> post('/quizz/delete/{id}', [QuizzController::class, 'deleteQuizz']);
+Route::middleware('auth:sanctum') -> post('/quizz/edit/{id}', [QuizzController::class, 'editQuizz']);
+Route::middleware('auth:sanctum') -> post('/quizz/add', [QuizzController::class, 'addQuizz']);
 
 //Questions API routes
 Route::get('/question/all/{quizzId}', [AnswerController::class, 'getAllQuestions']);
-Route::post('/question/create', [AnswerController::class, 'addQuestion']);
-Route::post('/question/delete', [AnswerController::class, 'deleteQuestion']);
-Route::post('/question/edit/{id}', [AnswerController::class, 'editQuestion']);
+Route::middleware('auth:sanctum') -> post('/question/create', [AnswerController::class, 'addQuestion']);
+Route::middleware('auth:sanctum') -> post('/question/delete', [AnswerController::class, 'deleteQuestion']);
+Route::middleware('auth:sanctum') -> post('/question/edit/{id}', [AnswerController::class, 'editQuestion']);
