90150ee6200e1fd68232812e85aa5a2195cd8e9b
krystiano13 <crystianotv@gmail.com>
2024-01-10 15:18:41 +0100

    create model for ratings

---
+++ server/app/Models/Rating.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = "id";
+
+    /**
+     * @var string
+     */
+    protected $table = "ratings";
+
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'username',
+        'rating_value'
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+}
