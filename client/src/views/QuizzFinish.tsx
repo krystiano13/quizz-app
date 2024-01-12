8b097a4a70609e67fd52532fb43bcd3f1e23e033
krystiano13 <crystianotv@gmail.com>
2024-01-12 12:24:11 +0100

    create styles and animations for quizzFinish View

--- client/src/views/QuizzFinish.tsx
+++ client/src/views/QuizzFinish.tsx
@@ -6,12 +6,20 @@ import { Button } from '../components/ui/button';
 
 export default function QuizzFinish() {
     return (
-        <main className="form-anim theme-rose w-[100vw] h-[100vh] flex flex-col justify-center p-20">
-            <h1 className="font-bold text-5xl">Quizz finished !</h1>
+        <main className="side-anim theme-rose w-[100vw] h-[100vh] flex flex-col justify-center p-20">
+            <h1 className="font-bold text-3xl md:text-5xl">Quizz finished !</h1>
             <Separator className="mt-6" />
-            <h2>Your Score: 77%</h2>
-            <h2>Your Rank: B</h2>
-            <Button className="w-1/5">Go Back</Button>
+            <h2 className="mt-6 font-semibold text-3xl">Your Score:
+                <span className="text-rose-600 text-4xl font-bold">
+                {" "}77%
+                </span>
+            </h2>
+            <h2 className="mt-6 font-semibold text-3xl">Your Rank:
+                <span className="text-rose-600 text-4xl font-bold">
+                {" "}B
+                </span>
+            </h2>
+            <Button className="w-3/5 md:w-1/5 mt-6">Go Back</Button>
         </main>
     )
 }
