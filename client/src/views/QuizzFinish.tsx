3979273c93411349f4a64d1c0fd734ee82665621
krystiano13 <crystianotv@gmail.com>
2024-01-12 12:18:47 +0100

    create structure of quizzFinish view

--- client/src/views/QuizzFinish.tsx
+++ client/src/views/QuizzFinish.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+//components
+import { Separator } from '../components/ui/separator';
+import { Button } from '../components/ui/button';
+
 export default function QuizzFinish() {
     return (
-        <></>
+        <main className="form-anim theme-rose w-[100vw] h-[100vh] flex flex-col justify-center p-20">
+            <h1 className="font-bold text-5xl">Quizz finished !</h1>
+            <Separator className="mt-6" />
+            <h2>Your Score: 77%</h2>
+            <h2>Your Rank: B</h2>
+            <Button className="w-1/5">Go Back</Button>
+        </main>
     )
 }
