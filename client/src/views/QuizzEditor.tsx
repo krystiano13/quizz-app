751fc221b4bb19dcde0c26f653723532f2b72f9d
krystiano13 <crystianotv@gmail.com>
2024-01-15 11:18:56 +0100

    create styles for desktop version of quizz editor

--- client/src/views/QuizzEditor.tsx
+++ client/src/views/QuizzEditor.tsx
@@ -23,8 +23,22 @@ export default function QuizzEditor() {
                     <Button className="max-w-[90%] w-3/5 text-lg">Delete Quizz</Button>
                 </form>
             </Card>
-            <Card className="w-[60%] h-[80%]" id="questions">
-
+            <Card className="w-[60%] h-[80%] flex flex-col gap-6 items-center p-4 overflow-y-auto" id="questions">
+                <Button variant="secondary" className="flex justify-between p-2 w-4/5 text-xl">
+                    Question 1
+                    <section className="flex gap-3">
+                        <Button>Edit</Button>
+                        <Button variant="destructive">Delete</Button>
+                    </section>
+                </Button>
+                <Button variant="secondary" className="flex justify-between p-2 w-4/5 text-xl">
+                    Question 2
+                    <section className="flex gap-3">
+                        <Button>Edit</Button>
+                        <Button variant="destructive">Delete</Button>
+                    </section>
+                </Button>
+                <Button className="w-4/5 text-xl">+</Button>
             </Card>
         </main>
     )
