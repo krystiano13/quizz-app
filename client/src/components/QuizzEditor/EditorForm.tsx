192c6e0b4629477a1c28dbea64b1ff56bde52bfa
krystiano13 <crystianotv@gmail.com>
2024-01-15 12:23:01 +0100

    implement choosing good answer in question editor form

--- client/src/components/QuizzEditor/EditorForm.tsx
+++ client/src/components/QuizzEditor/EditorForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //components
 import { Textarea } from "../ui/textarea";
@@ -7,6 +7,7 @@ import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 
 export function EditorForm() {
+    const [trueAnswer, setTrueAnswer] = useState<string>("a");
     return (
         <div className="side-anim theme-rose fixed z-50 bg-black bg-opacity-60 w-[100vw] h-[100vh] flex items-center justify-center">
             <Card className="p-8 w-[85%] lg:w-[40%]">
@@ -14,19 +15,47 @@ export function EditorForm() {
                     <Textarea required className="max-h-[10rem]" placeholder="question"></Textarea>
                     <section className="flex w-full">
                         <Input required placeholder="Answer A"/>
-                        <Button>True</Button>
+                        <Button
+                            type="button"
+                            onClick={() => setTrueAnswer("a")}
+                            variant={trueAnswer === "a" ? "default" : "secondary"}>
+                            {
+                                trueAnswer === "a" ? "True" : "False"
+                            }
+                        </Button>
                     </section>
                     <section className="flex w-full">
                         <Input required placeholder="Answer B"/>
-                        <Button>True</Button>
+                        <Button
+                            type="button"
+                            onClick={() => setTrueAnswer("b")}
+                            variant={trueAnswer === "b" ? "default" : "secondary"}>
+                            {
+                                trueAnswer === "b" ? "True" : "False"
+                            }
+                        </Button>
                     </section>
                     <section className="flex w-full">
                         <Input required placeholder="Answer C"/>
-                        <Button>True</Button>
+                        <Button
+                            type="button"
+                            onClick={() => setTrueAnswer("c")}
+                            variant={trueAnswer === "c" ? "default" : "secondary"}>
+                            {
+                                trueAnswer === "c" ? "True" : "False"
+                            }
+                        </Button>
                     </section>
                     <section className="flex w-full">
                         <Input required placeholder="Answer D"/>
-                        <Button>True</Button>
+                        <Button
+                            type="button"
+                            onClick={() => setTrueAnswer("d")}
+                            variant={trueAnswer === "d" ? "default" : "secondary"}>
+                            {
+                                trueAnswer === "d" ? "True" : "False"
+                            }
+                        </Button>
                     </section>
                     <Button className="w-full">Save</Button>
                 </form>
