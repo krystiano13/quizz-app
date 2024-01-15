ffcd92f4f914d6eba2f69e6986b12101dc4c38db
krystiano13 <crystianotv@gmail.com>
2024-01-15 12:18:53 +0100

    create styles for mobile version of edit form

--- client/src/components/QuizzEditor/EditorForm.tsx
+++ client/src/components/QuizzEditor/EditorForm.tsx
@@ -8,27 +8,27 @@ import { Card } from "../ui/card";
 
 export function EditorForm() {
     return (
-        <div className="side-anim theme-rose fixed z-50 bg-black bg-opacity-40 w-[100vw] h-[100vh] flex items-center justify-center">
-            <Card>
-                <form>
-                    <Textarea placeholder="question"></Textarea>
-                    <section className="flex">
-                        <Input placeholder="Answer A"/>
+        <div className="side-anim theme-rose fixed z-50 bg-black bg-opacity-60 w-[100vw] h-[100vh] flex items-center justify-center">
+            <Card className="p-8 w-[85%] lg:w-[40%]">
+                <form className="flex flex-col items-center gap-4 w-full">
+                    <Textarea required className="max-h-[10rem]" placeholder="question"></Textarea>
+                    <section className="flex w-full">
+                        <Input required placeholder="Answer A"/>
                         <Button>True</Button>
                     </section>
-                    <section className="flex">
-                        <Input placeholder="Answer B"/>
+                    <section className="flex w-full">
+                        <Input required placeholder="Answer B"/>
                         <Button>True</Button>
                     </section>
-                    <section className="flex">
-                        <Input placeholder="Answer C"/>
+                    <section className="flex w-full">
+                        <Input required placeholder="Answer C"/>
                         <Button>True</Button>
                     </section>
-                    <section className="flex">
-                        <Input placeholder="Answer D"/>
+                    <section className="flex w-full">
+                        <Input required placeholder="Answer D"/>
                         <Button>True</Button>
                     </section>
-                    <Button>Save</Button>
+                    <Button className="w-full">Save</Button>
                 </form>
             </Card>
         </div>
