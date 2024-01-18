342f57ab0c03c549bf7a81b2f7d958f5e71aa3d9
krystiano13 <crystianotv@gmail.com>
2024-01-18 11:13:18 +0100

    create api function for hiding quizz from others

--- server/app/Http/Controllers/QuizzController.php
+++ server/app/Http/Controllers/QuizzController.php
@@ -21,6 +21,30 @@ class QuizzController extends Controller
         ], 200);
     }
 
+    public function hideQuizz(int $id, Request $req) {
+        $validation = Validator::make($req -> all(),[
+            'username' => 'required'
+        ]);
+
+        if($validation -> fails()) {
+            return response([
+                'status' => false,
+                'errors' => $validation -> errors()
+            ], 403);
+        }
+
+        Quizz::where('id', $id)
+            -> where('author', $req -> get('username'))
+            -> update([
+                'hidden' => true
+            ]);
+
+        return response([
+            'status' => true,
+            'message' => 'Hidden'
+        ], 200);
+    }
+
     public function getHighestRated(Request $req) {
         $result = Quizz::where('hidden', false)
             -> orderByRaw('rating_sum / rates_count DESC')
